49afd0c40730874b4d990327907ff6dbf2e37376
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-28 14:50:19 +0000

    Fix import statements and add Redux store provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import './index.css';
+import './index.css'
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import App from './App.jsx';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import App from './App.jsx'
+import { Provider } from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { store } from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-);
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/*" element={<App />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+)
