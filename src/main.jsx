d5e297ab7a6d925f52bbc2d68f27f53849d0ac12
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-01-28 12:07:38 +0000

    Refactor routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
