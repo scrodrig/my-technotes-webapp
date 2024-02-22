b14528493ed4c911ac420a85683c060c2b7dae6d
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-22 17:20:48 +0000

    Refactor EditNote component to fetch note and users data

--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -1,7 +1,18 @@
-const EditNote = () => {
-  return (
-    <div>EditNote</div>
-  )
-}
+import EditNoteForm from './EditNoteForm'
+import { selectAllUsers } from '../users/usersApiSlice'
+import { selectNoteById } from './notesApiSlice'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
+const EditNote = () => {
+    const { id } = useParams()
+
+    const note = useSelector((state) => selectNoteById(state, id))
+    const users = useSelector(selectAllUsers)
+
+    const content =
+        note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
+
+    return content
+}
 export default EditNote
