9040464e61ac45f68d37f872e3864960a23596a9
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-22 17:20:37 +0000

    Add NewNoteForm and user data to NewNote component

--- src/features/notes/NewNote.jsx
+++ src/features/notes/NewNote.jsx
@@ -1,7 +1,12 @@
-const NewNote = () => {
-  return (
-    <div>NewNote</div>
-  )
-}
+import NewNoteForm from './NewNoteForm'
+import { selectAllUsers } from '../users/usersApiSlice'
+import { useSelector } from 'react-redux'
 
+const NewNote = () => {
+    const users = useSelector(selectAllUsers)
+
+    const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+
+    return content
+}
 export default NewNote
