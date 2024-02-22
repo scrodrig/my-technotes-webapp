218328935f63ab847fc116400b0480e9be967299
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-22 17:20:19 +0000

    Add user editing functionality

--- src/features/users/EditUser.jsx
+++ src/features/users/EditUser.jsx
@@ -1,5 +1,16 @@
+import EditUserForm from './EditUserForm'
+import { selectUserById } from './usersApiSlice'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
 const EditUser = () => {
-    return <div>EditUser</div>
+    const { id } = useParams()
+
+    const user = useSelector((state) => selectUserById(state, id))
+
+    const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+
+    return content
 }
 
 export default EditUser
