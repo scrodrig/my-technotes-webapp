fbdf916251a13a446e0161926631b39a4b15633a
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-22 17:19:32 +0000

    Remove unused data retention in usersApiSlice

--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -13,7 +13,6 @@ export const usersApiSlice = apiSlice.injectEndpoints({
             validateStaus: (response, result) => {
                 return response.status === 200 && !result.isError
             },
-            keepUnusedDataFor: 5,
             transformResponse: (responseData) => {
                 const loadedUsers = responseData.map((user) => {
                     user.id = user._id
