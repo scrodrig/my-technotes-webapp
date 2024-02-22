5874861b08d0ced6287973692cccb0316b7ffdb1
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-22 17:20:14 +0000

    Add polling and refetch options to useGetUsersQuery

--- src/features/users/UsersList.jsx
+++ src/features/users/UsersList.jsx
@@ -8,7 +8,11 @@ const UsersList = () => {
         isSuccess,
         isError,
         error,
-    } = useGetUsersQuery()
+    } = useGetUsersQuery(null, {
+        pollingInterval: 60000,
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true,
+    })
 
     let content
 
