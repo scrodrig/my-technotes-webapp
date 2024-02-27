f44078066eb43d9849b2627b229123025b640fc2
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-02-27 12:27:31 +0000

    Refactor DashHeader component to add new buttons and improve navigation

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,7 +1,14 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom'
+import {
+    faFileCirclePlus,
+    faFilePen,
+    faRightFromBracket,
+    faUserGear,
+    faUserPlus,
+} from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import useAuth from 'src/hooks/useAuth'
 import { useEffect } from 'react'
 import { useSendLogoutMutation } from '../features/auth/authApiSlice'
 
@@ -10,6 +17,7 @@ const NOTES_REGEX = /^\/dash\/notes(\/)?$/
 const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
+    const { isManager, isAdmin } = useAuth()
     const navigate = useNavigate()
     const { pathname } = useLocation()
 
@@ -22,9 +30,10 @@ const DashHeader = () => {
         }
     }, [isSuccess, navigate])
 
-    if (isLoading) return <p>Logging Out...</p>
-
-    if (isError) return <p>Error: {error.data?.message}</p>
+    const onNewNoteClicked = () => navigate('/dash/notes/new')
+    const onNewUserClicked = () => navigate('/dash/users/new')
+    const onNotesClicked = () => navigate('/dash/notes')
+    const onUsersClicked = () => navigate('/dash/users')
 
     let dashClass = null
     if (
@@ -35,24 +44,92 @@ const DashHeader = () => {
         dashClass = 'dash-header__container--small'
     }
 
+    let newNoteButton = null
+    if (NOTES_REGEX.test(pathname)) {
+        newNoteButton = (
+            <button
+                className="icon-button"
+                title="New Note"
+                onClick={onNewNoteClicked}>
+                <FontAwesomeIcon icon={faFileCirclePlus} />
+            </button>
+        )
+    }
+
+    let newUserButton = null
+    if (USERS_REGEX.test(pathname)) {
+        newUserButton = (
+            <button
+                className="icon-button"
+                title="New User"
+                onClick={onNewUserClicked}>
+                <FontAwesomeIcon icon={faUserPlus} />
+            </button>
+        )
+    }
+
+    let userButton = null
+    if (isManager || isAdmin) {
+        if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+            userButton = (
+                <button
+                    className="icon-button"
+                    title="Users"
+                    onClick={onUsersClicked}>
+                    <FontAwesomeIcon icon={faUserGear} />
+                </button>
+            )
+        }
+    }
+
+    let notesButton = null
+    if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
+        notesButton = (
+            <button
+                className="icon-button"
+                title="Notes"
+                onClick={onNotesClicked}>
+                <FontAwesomeIcon icon={faFilePen} />
+            </button>
+        )
+    }
+
     const logoutButton = (
         <button className="icon-button" title="Logout" onClick={sendLogout}>
             <FontAwesomeIcon icon={faRightFromBracket} />
         </button>
     )
 
+    const errClass = isError ? 'errmsg' : 'offscreen'
+
+    let buttonContent
+    if (isLoading) {
+        buttonContent = <p>Logging Out...</p>
+    } else {
+        buttonContent = (
+            <>
+                {newNoteButton}
+                {newUserButton}
+                {notesButton}
+                {userButton}
+                {logoutButton}
+            </>
+        )
+    }
+
     const content = (
-        <header className="dash-header">
-            <div className={`dash-header__container ${dashClass}`}>
-                <Link to="/dash">
-                    <h1 className="dash-header__title">techNotes</h1>
-                </Link>
-                <nav className="dash-header__nav">
-                    {/* add more buttons later */}
-                    {logoutButton}
-                </nav>
-            </div>
-        </header>
+        <>
+            <p className={errClass}>{error?.data?.message}</p>
+
+            <header className="dash-header">
+                <div className={`dash-header__container ${dashClass}`}>
+                    <Link to="/dash">
+                        <h1 className="dash-header__title">techNotes</h1>
+                    </Link>
+                    <nav className="dash-header__nav">{buttonContent}</nav>
+                </div>
+            </header>
+        </>
     )
 
     return content
