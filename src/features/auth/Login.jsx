665ad3a58c9dc06ead824a0c927def2e78a44cbd
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 15:04:52 +0000

    Add PulseLoader component to show loading spinner

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useEffect, useRef, useState } from 'react'
 
+import PulseLoader from 'react-spinners/PulseLoader'
 import { setCredentials } from './authSlice'
 import { useDispatch } from 'react-redux'
 import { useLoginMutation } from './authApiSlice'
@@ -55,7 +56,7 @@ const Login = () => {
 
     const errClass = errMsg ? 'errmsg' : 'offscreen'
 
-    if (isLoading) return <p>Loading...</p>
+    if (isLoading) return <PulseLoader color={'#FFF'} />
 
     const content = (
         <section className="public">
