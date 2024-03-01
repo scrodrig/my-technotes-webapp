424b57bb411cc161e1e9653356b3a2f12a2c2009
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 16:05:11 +0000

    Refactor NewUserForm component

--- src/features/users/NewUserForm.jsx
+++ src/features/users/NewUserForm.jsx
@@ -5,12 +5,16 @@ import { ROLES } from '../../config/roles'
 import { faSave } from '@fortawesome/free-solid-svg-icons'
 import { useAddNewUserMutation } from './usersApiSlice'
 import { useNavigate } from 'react-router-dom'
+import useTitle from '../../hooks/useTitle'
 
 const USER_REGEX = /^[A-z]{3,20}$/
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/
 
 const NewUserForm = () => {
-    const [addNewUser, { isLoading, isSuccess, isError, error }] = useAddNewUserMutation()
+    useTitle('techNotes: New User')
+
+    const [addNewUser, { isLoading, isSuccess, isError, error }] =
+        useAddNewUserMutation()
 
     const navigate = useNavigate()
 
@@ -43,12 +47,14 @@ const NewUserForm = () => {
     const onRolesChanged = (e) => {
         const values = Array.from(
             e.target.selectedOptions, //HTMLCollection
-            (option) => option.value,
+            (option) => option.value
         )
         setRoles(values)
     }
 
-    const canSave = [roles.length, validUsername, validPassword].every(Boolean) && !isLoading
+    const canSave =
+        [roles.length, validUsername, validPassword].every(Boolean) &&
+        !isLoading
 
     const onSaveUserClicked = async (e) => {
         e.preventDefault()
@@ -70,7 +76,9 @@ const NewUserForm = () => {
     const validUserClass = !validUsername ? 'form__input--incomplete' : ''
     const validPwdClass = !validPassword ? 'form__input--incomplete' : ''
     // eslint-disable-next-line no-extra-boolean-cast
-    const validRolesClass = !Boolean(roles.length) ? 'form__input--incomplete' : ''
+    const validRolesClass = !Boolean(roles.length)
+        ? 'form__input--incomplete'
+        : ''
 
     const content = (
         <>
@@ -80,7 +88,10 @@ const NewUserForm = () => {
                 <div className="form__title-row">
                     <h2>New User</h2>
                     <div className="form__action-buttons">
-                        <button className="icon-button" title="Save" disabled={!canSave}>
+                        <button
+                            className="icon-button"
+                            title="Save"
+                            disabled={!canSave}>
                             <FontAwesomeIcon icon={faSave} />
                         </button>
                     </div>
@@ -99,7 +110,8 @@ const NewUserForm = () => {
                 />
 
                 <label className="form__label" htmlFor="password">
-                    Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+                    Password:{' '}
+                    <span className="nowrap">[4-12 chars incl. !@#$%]</span>
                 </label>
                 <input
                     className={`form__input ${validPwdClass}`}
@@ -120,8 +132,7 @@ const NewUserForm = () => {
                     multiple={true}
                     size="3"
                     value={roles}
-                    onChange={onRolesChanged}
-                >
+                    onChange={onRolesChanged}>
                     {options}
                 </select>
             </form>
@@ -130,5 +141,4 @@ const NewUserForm = () => {
 
     return content
 }
-
 export default NewUserForm
