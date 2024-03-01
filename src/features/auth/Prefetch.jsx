91e5cf398914a3cf42e2298af17b7f22e4ed726f
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 12:56:09 +0000

    Refactor Prefetch component to use react-router-dom and optimize data fetching

--- src/features/auth/Prefetch.jsx
+++ src/features/auth/Prefetch.jsx
@@ -1,20 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom'
 import { notesApiSlice } from '../notes/notesApiSlice'
 import { store } from '../../app/store'
-import { useEffect } from 'react';
-import { usersApiSlice } from '../users/usersApiSlice';
+import { useEffect } from 'react'
+import { usersApiSlice } from '../users/usersApiSlice'
 
 const Prefetch = () => {
     useEffect(() => {
-        console.log('subscribing')
-        const notes = store.dispatch(notesApiSlice.endpoints.getNotes.initiate())
-        const users = store.dispatch(usersApiSlice.endpoints.getUsers.initiate())
-
-        return () => {
-            console.log('unsubscribing')
-            notes.unsubscribe()
-            users.unsubscribe()
-        }
+        store.dispatch(
+            notesApiSlice.util.prefetch('getNotes', 'notesList', {
+                force: true,
+            })
+        )
+        store.dispatch(
+            usersApiSlice.util.prefetch('getUsers', 'usersList', {
+                force: true,
+            })
+        )
     }, [])
 
     return <Outlet />
