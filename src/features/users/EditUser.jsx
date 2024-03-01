d252f14da3dbff934b82b5eb969135a4f6b04457
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 12:56:42 +0000

    Refactor EditUser component to use useGetUsersQuery hook

--- src/features/users/EditUser.jsx
+++ src/features/users/EditUser.jsx
@@ -1,14 +1,19 @@
 import EditUserForm from './EditUserForm'
-import { selectUserById } from './usersApiSlice'
+import PulseLoader from 'react-spinners/PulseLoader'
+import { useGetUsersQuery } from 'src/features/users/usersApiSlice'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 const EditUser = () => {
     const { id } = useParams()
 
-    const user = useSelector((state) => selectUserById(state, id))
+    const { user } = useGetUsersQuery('noteList', {
+        selectFromResult: ({ data }) => ({
+            user: data?.entities[id],
+        }),
+    })
 
-    const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>
+    if (!user) return <PulseLoader color={'#FFF'} />
+    const content = <EditUserForm user={user} />
 
     return content
 }
