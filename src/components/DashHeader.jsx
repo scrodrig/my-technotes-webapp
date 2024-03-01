e0f71424006a144b25828667745f6a80fd6af441
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 09:32:51 +0000

    Refactor DashHeader component

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,10 +1,10 @@
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import {
-    faFileCirclePlus,
-    faFilePen,
-    faRightFromBracket,
-    faUserGear,
-    faUserPlus,
+  faFileCirclePlus,
+  faFilePen,
+  faRightFromBracket,
+  faUserGear,
+  faUserPlus,
 } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -17,127 +17,121 @@ const NOTES_REGEX = /^\/dash\/notes(\/)?$/
 const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
-    const { isManager, isAdmin } = useAuth()
-    const navigate = useNavigate()
-    const { pathname } = useLocation()
+  const { isManager, isAdmin } = useAuth()
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
 
-    const [sendLogout, { isLoading, isSuccess, isError, error }] =
-        useSendLogoutMutation()
+  const [sendLogout, { isLoading, isSuccess, isError, error }] =
+    useSendLogoutMutation()
 
-    useEffect(() => {
-        if (isSuccess) {
-            navigate('/')
-        }
-    }, [isSuccess, navigate])
-
-    const onNewNoteClicked = () => navigate('/dash/notes/new')
-    const onNewUserClicked = () => navigate('/dash/users/new')
-    const onNotesClicked = () => navigate('/dash/notes')
-    const onUsersClicked = () => navigate('/dash/users')
-
-    let dashClass = null
-    if (
-        !DASH_REGEX.test(pathname) &&
-        !NOTES_REGEX.test(pathname) &&
-        !USERS_REGEX.test(pathname)
-    ) {
-        dashClass = 'dash-header__container--small'
+  useEffect(() => {
+    if (isSuccess) {
+      navigate('/')
     }
+  }, [isSuccess, navigate])
 
-    let newNoteButton = null
-    if (NOTES_REGEX.test(pathname)) {
-        newNoteButton = (
-            <button
-                className="icon-button"
-                title="New Note"
-                onClick={onNewNoteClicked}>
-                <FontAwesomeIcon icon={faFileCirclePlus} />
-            </button>
-        )
-    }
+  const onNewNoteClicked = () => navigate('/dash/notes/new')
+  const onNewUserClicked = () => navigate('/dash/users/new')
+  const onNotesClicked = () => navigate('/dash/notes')
+  const onUsersClicked = () => navigate('/dash/users')
 
-    let newUserButton = null
-    if (USERS_REGEX.test(pathname)) {
-        newUserButton = (
-            <button
-                className="icon-button"
-                title="New User"
-                onClick={onNewUserClicked}>
-                <FontAwesomeIcon icon={faUserPlus} />
-            </button>
-        )
-    }
+  let dashClass = null
+  if (
+    !DASH_REGEX.test(pathname) &&
+    !NOTES_REGEX.test(pathname) &&
+    !USERS_REGEX.test(pathname)
+  ) {
+    dashClass = 'dash-header__container--small'
+  }
 
-    let userButton = null
-    if (isManager || isAdmin) {
-        if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
-            userButton = (
-                <button
-                    className="icon-button"
-                    title="Users"
-                    onClick={onUsersClicked}>
-                    <FontAwesomeIcon icon={faUserGear} />
-                </button>
-            )
-        }
-    }
+  let newNoteButton = null
+  if (NOTES_REGEX.test(pathname)) {
+    newNoteButton = (
+      <button
+        className="icon-button"
+        title="New Note"
+        onClick={onNewNoteClicked}>
+        <FontAwesomeIcon icon={faFileCirclePlus} />
+      </button>
+    )
+  }
 
-    let notesButton = null
-    if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
-        notesButton = (
-            <button
-                className="icon-button"
-                title="Notes"
-                onClick={onNotesClicked}>
-                <FontAwesomeIcon icon={faFilePen} />
-            </button>
-        )
-    }
+  let newUserButton = null
+  if (USERS_REGEX.test(pathname)) {
+    newUserButton = (
+      <button
+        className="icon-button"
+        title="New User"
+        onClick={onNewUserClicked}>
+        <FontAwesomeIcon icon={faUserPlus} />
+      </button>
+    )
+  }
 
-    const logoutButton = (
-        <button
-            className="icon-button"
-            title="Logout"
-            onClick={() => {
-                sendLogout()
-                navigate('/')
-            }}>
-            <FontAwesomeIcon icon={faRightFromBracket} />
+  let userButton = null
+  if (isManager || isAdmin) {
+    if (!USERS_REGEX.test(pathname) && pathname.includes('/dash')) {
+      userButton = (
+        <button className="icon-button" title="Users" onClick={onUsersClicked}>
+          <FontAwesomeIcon icon={faUserGear} />
         </button>
-    )
-
-    const errClass = isError ? 'errmsg' : 'offscreen'
-
-    let buttonContent
-    if (isLoading) {
-        buttonContent = <p>Logging Out...</p>
-    } else {
-        buttonContent = (
-            <>
-                {newNoteButton}
-                {newUserButton}
-                {notesButton}
-                {userButton}
-                {logoutButton}
-            </>
-        )
+      )
     }
+  }
 
-    const content = (
-        <>
-            <p className={errClass}>{error?.data?.message}</p>
-
-            <header className="dash-header">
-                <div className={`dash-header__container ${dashClass}`}>
-                    <Link to="/dash">
-                        <h1 className="dash-header__title">techNotes</h1>
-                    </Link>
-                    <nav className="dash-header__nav">{buttonContent}</nav>
-                </div>
-            </header>
-        </>
+  let notesButton = null
+  if (!NOTES_REGEX.test(pathname) && pathname.includes('/dash')) {
+    notesButton = (
+      <button className="icon-button" title="Notes" onClick={onNotesClicked}>
+        <FontAwesomeIcon icon={faFilePen} />
+      </button>
     )
+  }
 
-    return content
+  const logoutButton = (
+    <button
+      className="icon-button"
+      title="Logout"
+      onClick={() => {
+        sendLogout()
+        navigate('/')
+      }}>
+      <FontAwesomeIcon icon={faRightFromBracket} />
+    </button>
+  )
+
+  const errClass = isError ? 'errmsg' : 'offscreen'
+
+  let buttonContent
+  if (isLoading) {
+    buttonContent = <p>Logging Out...</p>
+  } else {
+    buttonContent = (
+      <>
+        {newNoteButton}
+        {newUserButton}
+        {notesButton}
+        {userButton}
+        {logoutButton}
+      </>
+    )
+  }
+
+  const content = (
+    <>
+      <p className={errClass}>{error?.data?.message}</p>
+
+      <header className="dash-header">
+        <div className={`dash-header__container ${dashClass}`}>
+          <Link to="/dash">
+            <h1 className="dash-header__title">techNotes</h1>
+          </Link>
+          <nav className="dash-header__nav">{buttonContent}</nav>
+        </div>
+      </header>
+    </>
+  )
+
+  return content
 }
 export default DashHeader
