12e9f951cf619ea007951580073b2a784817a4d8
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-03-01 12:56:38 +0000

    Add loading spinner and access control to EditNote component

--- src/features/notes/EditNote.jsx
+++ src/features/notes/EditNote.jsx
@@ -1,17 +1,36 @@
 import EditNoteForm from './EditNoteForm'
-import { selectAllUsers } from '../users/usersApiSlice'
-import { selectNoteById } from './notesApiSlice'
+import PulseLoader from 'react-spinners/PulseLoader'
+import useAuth from '../../hooks/useAuth'
+import { useGetNotesQuery } from './notesApiSlice'
+import { useGetUsersQuery } from '../users/usersApiSlice'
 import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
 
 const EditNote = () => {
     const { id } = useParams()
 
-    const note = useSelector((state) => selectNoteById(state, id))
-    const users = useSelector(selectAllUsers)
+    const { username, isManager, isAdmin } = useAuth()
 
-    const content =
-        note && users ? <EditNoteForm note={note} users={users} /> : <p>Loading...</p>
+    const { note } = useGetNotesQuery('notesList', {
+        selectFromResult: ({ data }) => ({
+            note: data?.entities[id],
+        }),
+    })
+
+    const { users } = useGetUsersQuery('usersList', {
+        selectFromResult: ({ data }) => ({
+            users: data?.ids.map((id) => data?.entities[id]),
+        }),
+    })
+
+    if (!note || !users?.length) return <PulseLoader color={'#FFF'} />
+
+    if (!isManager && !isAdmin) {
+        if (note.username !== username) {
+            return <p className="errmsg">No access</p>
+        }
+    }
+
+    const content = <EditNoteForm note={note} users={users} />
 
     return content
 }
